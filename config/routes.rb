a913c4d9c9fa00c93698898c3a0830675b4c9bcc
pat-crane <pkc999@icloud.com>
2026-01-14 22:52:22 -0500

    basic styles and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # static pages
+  get "home", to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
+
 # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
 # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
 # Defines the root path route ("/")
-root "projects#index"
+root "pages#home"
 end
